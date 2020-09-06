f1b9f172737e60643b6b2ae1039747ede7085d63
shkurkohalyna <krysty1105@gmail.com>
2020-09-06 21:38:49 +0300

    endtask

--- js/task-2.js
+++ js/task-2.js
@@ -2,13 +2,14 @@ const inventory = {
   items: ["Knife", "Gas mask"],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
-
-    items.push(itemName);
+    this.items.push(itemName);
+    return `Adding ${itemName} to inventory`;
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
     this.items = this.items.filter((item) => item !== itemName);
+    return `Removing ${itemName} from inventory`;
   },
 };
 
@@ -17,14 +18,37 @@ const invokeInventoryAction = function (itemName, action) {
   action(itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add);
+invokeInventoryAction("Medkit", inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
+console.log(inventory.items);
+// ['Knife', 'Gas mask', 'Medkit']
 
-console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-
-invokeInventoryAction("Gas mask", inventory.remove);
-// Invoking action on Gas mask
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory)); // Invoking action on Gas mask
 // Removing Gas mask from inventory
-
 console.log(inventory.items); // ['Knife', 'Medkit']
+
+// const invokeInventoryAction = function (itemName, action) {
+//     const act = action(itemName);
+//     const msg = `Invoking action on ${itemName}`;
+//     return { act, msg };
+// };
+
+// const invokeAdd = invokeInventoryAction(
+//     'Medkit',
+//     inventory.add.bind(inventory)
+// );
+// const arrayAdd = [...inventory.items];
+
+// console.log(invokeAdd);
+// console.log(arrayAdd);
+
+// const invokeRemove = invokeInventoryAction(
+//     'Gas mask',
+//     inventory.remove.bind(inventory)
+// );
+
+// const arrayRemove = [...inventory.items];
+
+// console.log(invokeRemove);
+// console.log(arrayRemove);
